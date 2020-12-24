243cec193c41603c851dbb749da155fd770b97c2
Jonathan Hoyle <jonathan.hoyle@ruralsourcing.com>
2020-12-24 15:41:36 -0500

    Just some stuff

--- day10.py
+++ day10.py
@@ -1,2 +1,8 @@
 with open('day10.txt') as f:
     adapters = [int(l) for l in f.read().split('\n')]
+adapters = sorted(adapters)
+differences = [0,0,0,0]
+for i in range(len(adapters)-1):
+    differences[adapters[i+1] - adapters[i]] += 1
+print(adapters[0])
+print((differences[1] + 1) * differences[3])
--- day11.py
+++ day11.py
@@ -1,2 +1,5 @@
 with open('day11.txt') as f:
     lines = f.read().split('\n')
+def sp(s):
+    return [char for char in s]
+current_map = [sp(s) for s in lines]
